93e6b79b9bec43afb1d71fc1e0a323d26f00f78c
MohammedZ666 <abuobaidazishan@gmail.com>
2022-10-21 20:42:17 +0600

    video page fixed

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,42 +1,3 @@
-// import React from "react";
-// import { Stack, Box } from "@mui/material";
-
-// import { ChannelCard, Loader, VideoCard } from "./";
-
-// const Videos = ({ videos, direction }) => {
-//   if (!videos || videos.length === 0)
-//     return <Loader />;
-//   console.log(videos)
-//   return (
-//     <div >
-//       {videos.map((item, idx) => (
-//         <div key={idx}>
-//           <span>Test</span>
-//           {item && <span>{item.id}</span>}
-//         </div>))}
-//     </div>
-//   );
-// }
-
-// export default Videos;
-
-
-
-
-
-//<Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
-  // {videos.map((item, idx) => (
-  //   <Box key={idx}>
-  //     {item && item.id &&
-  //       <div>
-  //         {item.id.videoId && <VideoCard video={item} />}
-  //         {item.id.channelId && <ChannelCard channelDetail={item} />}
-  //       </div>
-  //     }
-  //   </Box>
-  // ))}
-//</Stack>
-
 import React from "react";
 import { Stack, Box } from "@mui/material";
 
@@ -46,7 +7,13 @@ const Videos = ({ videos, direction }) => {
   if (!videos?.length) return <Loader />;
 
   return (
-    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
@@ -55,6 +22,6 @@ const Videos = ({ videos, direction }) => {
       ))}
     </Stack>
   );
-}
+};
 
 export default Videos;
